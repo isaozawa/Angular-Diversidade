89e3fb27f1eebafb757b381f291c76997c5950a1
Isabelle <isa_ozawa@hotmail.com>
2021-01-20 13:01:49 -0300

    Home, menu, rodapé e contato

--- ProjetoDiversidade/src/app/app-routing.module.ts
+++ ProjetoDiversidade/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'}, 
+  {path: 'home', component: HomeComponent},
+  {path: 'contato', component: ContatoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
